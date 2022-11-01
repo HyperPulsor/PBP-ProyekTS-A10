fb09415993923ffa0ab639c92d845d3365af19a2
vinsensius.ferdinando <vinsensius.ferdinando@ui.ac.id>
2022-11-01 22:25:48 +0700

    connected kategori, product page, implemented ajax

--- katalog/urls.py
+++ katalog/urls.py
@@ -3,6 +3,8 @@ from katalog.views import *
 
 app_name = 'katalog'
 urlpatterns = [
-    path('show_produk/', show_produk_html, name='show_produk'),
-    path('show_toko/', show_toko_html, name='show_toko'),
+    path('show_produk/<int:id>/', show_produk_html_1, name='show_produk_detail'),
+    path('show_toko/<str:nama>/<str:deskripsi>/', show_toko_html_1, name='show_toko_kategori'),
+    path("show-json-produk/<int:id>/", show_json_produk, name='show-json-produk'),
+    path("like/<str:nama_produk>/", like_ajax_submit, name = "like")
 ]
