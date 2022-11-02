0832fce67304dbf921981407f77ebc0d153ce31a
hyperpulsor <rakanfasya@gmail.com>
2022-11-02 16:59:06 +0700

    Feat: Added Card hover and forum fix

--- forum/urls.py
+++ forum/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('json/<str:kategori_inputuser>/', show_json, name='show_json'),
     path('json_reply/<int:forum_id>/', show_json_reply, name='show_json_reply'),
     path('delete/<int:forum_id>', delete_forum, name='delete_forum'),
+    path('/addforum', add_forum, name='add_forum'),
+    path('/addreply', add_reply, name='add_reply'),
 ]
